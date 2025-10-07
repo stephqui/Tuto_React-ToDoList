e62b17682a69db17c887990d83ad4b22c440c119
stephqui <paradivuiz@gmail.com>
2025-10-07 12:01:09 +0200

    Lien Container Input pour titre de tache

--- src/components/taskContainer.jsx
+++ src/components/taskContainer.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Footer } from "./footer/footer";
 import { Header } from "./header/header";
 import { TaskInput } from "./taskInput/taskInput";
@@ -5,10 +6,39 @@ import { TaskList } from "./taskList/taskList";
 
 // Ce composant est utilisé pour afficher l'intégralité de la fonctionalité de Tache.
 export const TaskContainer = () => {
+  const [taskList, setTaskList] = useState([
+    /*{
+      id: 1,
+      title: "Faire la vaisselle",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "Sortir le chien",
+      completed: true,
+    },
+    {
+      id: 3,
+      title: "Sortir le chien",
+      completed: true,
+    },*/
+  ]);
+
+  console.log("taskList : >> ", taskList);
+
+  const addTask = (titleToAdd) => {
+    const newTask = {
+      id: taskList.length + 1,
+      title: titleToAdd,
+      completed: false,
+    };
+    setTaskList([...taskList, newTask]);
+  };
+
   return (
     <main>
       <Header />
-      <TaskInput />
+      <TaskInput addATask={addTask} />
       <TaskList />
       <Footer />
     </main>
--- src/components/taskInput/taskInput.jsx
+++ src/components/taskInput/taskInput.jsx
@@ -1,15 +1,32 @@
 // Ce composant est utilisé pour afficher le champ de saisie de tâche.
+import { useState } from "react";
 import styles from "./TaskInput.module.css";
 
-export const TaskInput = () => {
+export const TaskInput = ({ addATask }) => {
+  const [taskTitle, setTaskTitle] = useState("");
+
+  const handleInputChange = (e) => {
+    setTaskTitle(e.target.value);
+  };
+
+  const handleAddTask = (e) => {
+    e.preventDefault();
+    if (taskTitle === "") {
+    }
+    addATask(taskTitle);
+    setTaskTitle("");
+  };
+
   return (
     <div className={`box ${styles.element}`}>
       <h2 className={styles.title}>Ajoute ta prochaine tache</h2>
-      <form className={styles.container}>
+      <form className={styles.container} onSubmit={handleAddTask}>
         <input
           type="text"
           className={styles.input}
           placeholder="Indiquez un titre de tâche explicite"
+          onChange={handleInputChange}
+          value={taskTitle} //pour vider le champ après envoi
         />
         <button className="button-primary" type="submit">
           Ajouter
